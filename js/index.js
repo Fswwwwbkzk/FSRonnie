1f126b8a61fd7e0a26f5413411a56bd6d4c17eb5
Fswwwwbkzk <johnny2go223@gmail.com>
2024-08-30 15:23:16 +0800

    fixed: 更新

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 
-function loadHTML(id, url) {
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById(id).innerHTML = data;
-        });
-}
+// function loadHTML(id, url) {
+//     fetch(url)
+//         .then(response => response.text())
+//         .then(data => {
+//             document.getElementById(id).innerHTML = data;
+//         });
+// }
 
-document.addEventListener('DOMContentLoaded', function () {
-    loadHTML('header', 'header.html');
-    loadHTML('footer', 'footer.html');
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     loadHTML('header', 'header.html');
+//     loadHTML('footer', 'footer.html');
+// });
 
 
 
